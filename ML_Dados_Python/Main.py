e1abf328e9fc6997e9b7b3ac9672014d7cd9cbab
Arthur Rosado <aarthurrosado@gmail.com>
2025-06-03 15:52:21 -0300

    Create Main.py

---
+++ ML_Dados_Python/Main.py
@@ -0,0 +1,12 @@
+
+import streamlit as st
+
+st.set_page_config(
+    page_title='Sistema de deslizamentos',
+    page_icon='',
+    layout='wide'
+)
+st.title('Analise de deslizamentos')
+
+st.write("Informações sobre o sistema:")
+st.markdown("Esse sistema foi criado com o intuito de prever e informar possiveis catastrofes e emitir alertas do risco de deslizamento em determinada região por meio de um modelo preditivo.")
